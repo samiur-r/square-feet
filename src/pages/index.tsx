8b38b5bbc8fba2d5bb185127b1b51423f4d59ccb
samiur-r <samiurrahman.dev@gmail.com>
2022-10-06 05:18:15 +0600

    chore: add eslint wth airbnb and prettier #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      Boshamlan
-    </div>
-  )
+  return <div>Boshamlan</div>
 }
 
 export default Home
