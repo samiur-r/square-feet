aac4c5cfb5fa3b7fd994a807b3408ec334bbb121
samiur-r <samiurrahman.dev@gmail.com>
2022-10-08 00:16:42 +0600

    feat: add zustand for state management

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import {
   Hydrate,
   QueryClient,
@@ -11,17 +10,25 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { useCreateStore, StoreProvider } from '../store'
+
 const MyApp = ({
   Component,
   pageProps
-}: AppProps<{ dehydratedState: DehydratedState }>) => {
+}: AppProps<{
+  initialZustandState: { lastUpdate: number; light: boolean; count: number }
+  dehydratedState: DehydratedState
+}>) => {
+  const createStore = useCreateStore(pageProps.initialZustandState)
   const [queryClient] = React.useState(() => new QueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <Component {...pageProps} />
+        <StoreProvider createStore={createStore}>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Component {...pageProps} />
+        </StoreProvider>
       </Hydrate>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
