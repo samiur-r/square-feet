f5252643b1c0e1556b38422b845c2ca97b3e93cb
samiur-r <samiurrahman.dev@gmail.com>
2022-10-24 20:23:10 +0600

    feat: add faq

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Banner from 'components/Home/Banner'
 import Guide from 'components/Articles/Guide'
 import Cards from 'components/Home/Cards'
 import ApartmentRent from 'components/Articles/ApartmentRent'
+import Faq from 'components/Articles/Faq'
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +17,7 @@ const Home: NextPage = () => {
       <Guide />
       <Cards />
       <ApartmentRent />
+      <Faq />
     </>
   )
 }
