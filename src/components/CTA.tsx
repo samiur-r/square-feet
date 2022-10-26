c561db8295f5ca5f5b40d5ea4d834bdcefe9ba21
samiur-r <samiurrahman.dev@gmail.com>
2022-10-26 18:52:24 +0600

    feat: create homepage

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -4,15 +4,23 @@ interface CTAProps {
   title: string
   backgroundColor: string
   Icon?: JSX.Element
+  fullWidth?: boolean
 }
 
-const CTA: React.FC<CTAProps> = ({ title, backgroundColor, Icon }) => {
+const CTA: React.FC<CTAProps> = ({
+  title,
+  backgroundColor,
+  Icon,
+  fullWidth
+}) => {
   return (
     <button
       type="button"
       className={`${
         backgroundColor === 'primary' ? 'bg-primary' : 'bg-secondary'
-      } text-white opacity-80 font-medium py-3 px-8 md:rounded-lg rounded-full inline-flex items-center`}
+      } ${
+        fullWidth && 'w-full md:w-auto mx-10 md:mx-auto'
+      } text-white opacity-80 font-medium py-3 px-8 md:rounded-lg rounded-full flex justify-center items-center`}
     >
       <span>{title}</span>
       {Icon && Icon}
