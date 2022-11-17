25ac042a7d2e4e98c95f6c4feb9bc0a94b7cf610
samiur-r <samiurrahman.dev@gmail.com>
2022-11-17 14:56:38 +0600

    test: web performance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
+import dynamic from 'next/dynamic'
 
-import SearchBox from 'components/SearchBox'
 import Hero from 'components/Home/Hero'
 import Banner from 'components/Home/Banner'
 import Guide from 'components/Articles/Guide'
@@ -8,12 +8,19 @@ import Cards from 'components/Home/Cards'
 import ApartmentRent from 'components/Articles/ApartmentRent'
 import Faq from 'components/Articles/Faq'
 import Posts from 'components/Posts'
+import { Suspense } from 'react'
+
+const DynamicSearchBox = dynamic(() => import('components/SearchBox'), {
+  suspense: true
+})
 
 const Home: NextPage = () => {
   return (
     <div className="bg-gray-50 md:bg-white">
       <Hero />
-      <SearchBox />
+      <Suspense fallback="Loading...">
+        <DynamicSearchBox />
+      </Suspense>
       <Banner />
       <Posts />
       <Guide />
