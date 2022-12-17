92d299ad7993cd9a92ff0f005d4a92db89ef5551
samiur-r <samiurrahman.dev@gmail.com>
2022-12-18 02:52:07 +0600

    test: breaking font test

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,9 +5,7 @@ const Title: React.FC<{ value: string; light?: boolean }> = ({
   light
 }) => {
   return (
-    <h3 className={`${light && 'text-white'} text-lg md:text-xl tracking-wide`}>
-      {value}
-    </h3>
+    <h3 className={`${light && 'text-white'} text-lg md:text-xl`}>{value}</h3>
   )
 }
 
