69a9dd1cb625869ffda8dd7d6a87d4b2b4ddff02
samiur-r <samiurrahman.dev@gmail.com>
2023-01-02 01:43:36 +0600

    test: scroll pos fixes for autocomplete'

--- src/pages/post/create.tsx
+++ src/pages/post/create.tsx
@@ -977,6 +977,11 @@ const CreatePost: NextPage = () => {
   useEffect(() => {
     if (scrollToTop) {
       window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      })
+      window.scrollTo({
         top: 200,
         left: 0,
         behavior: 'smooth'
