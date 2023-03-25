91c3e0ddd1bbf7489088455a72b352d568648ea4
samiur-r <samiurrahman.dev@gmail.com>
2023-03-25 23:57:07 +0600

    chore: restore layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,31 +3,21 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import Layout from 'components/Layout'
 import { useCreateStore, Provider, StoreState } from 'store'
-import AdminLayout from 'components/Admin/AdminLayout'
 
 const MyApp = ({
   Component,
-  pageProps,
-  router
+  pageProps
 }: AppProps<{
   initialZustandState: StoreState
 }>) => {
   const createStore = useCreateStore(pageProps.initialZustandState)
-  const adminPanel = !!router.route.startsWith('/admin')
-  const getLayout = adminPanel
-    ? (
-        page: any
-        // eslint-disable-next-line react/no-children-prop
-      ) => <AdminLayout children={page} />
-    : (
-        page: any
-        // eslint-disable-next-line react/no-children-prop
-      ) => <Layout children={page} />
 
   return (
     <Provider createStore={createStore}>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      {getLayout(<Component {...pageProps} />)}
+      <Layout>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   )
 }
