0e7b318924063e54c2232eec53e3af2d569d6bf3
samiur-r <samiurrahman.dev@gmail.com>
2023-05-18 15:39:51 +0600

    fix: lint issues

--- src/pages/topup.tsx
+++ src/pages/topup.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import Router, { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import Image from 'next/image'
 
