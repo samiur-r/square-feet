b68717673ea395a4c336dde169cbb99dfcbb5cb2
“samiur-r” <samiurrahman.dev@gmail.com>
2023-06-22 04:43:46 +0600

    fix: filter modal fixes

--- src/components/SearchBox/FilterModal.tsx
+++ src/components/SearchBox/FilterModal.tsx
@@ -26,6 +26,7 @@ const FilterModal: React.FC<FilterModalProps> = ({
     propertyTypeSelected,
     categorySelected,
     priceRangeSelected,
+    keyword: lastKeyword,
     setPropertyTypeSelected,
     setCategorySelected,
     setPriceRangeSelected,
@@ -85,11 +86,11 @@ const FilterModal: React.FC<FilterModalProps> = ({
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0"
+              enterTo="opacity-100 translate-y-0"
               leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0"
             >
               <Dialog.Panel className="relative transform overflow-hidden md:rounded-lg bg-white pt-5 pb-4 shadow-xl transition-all h-screen md:h-auto md:my-8 w-full md:max-w-lg">
                 <div className="px-4">
@@ -263,6 +264,7 @@ const FilterModal: React.FC<FilterModalProps> = ({
                         className="outline-none text-base block px-4 py-3 md:py-4 pr-10 w-full text-black rounded-full border border-custom-gray-border text-end"
                         placeholder="البحث بالكلمه"
                         onChange={(e) => setKeyword(e.target.value)}
+                        defaultValue={lastKeyword}
                       />
                       <div className="flex absolute inset-y-0 right-5 items-center pl-2 pointer-events-none">
                         <svg
